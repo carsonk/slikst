fac41adb9b2a92f4d087c3625576ee72d4a982a0
Carson Killbreath <kill4992@kettering.edu>
2015-07-08 17:17:40 -0400

    Simple school creation page for admins.

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -1,5 +1,9 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use App\School;
+
 class SchoolController extends Controller {
     /**
      * Create a new controller instance.
@@ -8,7 +12,7 @@ class SchoolController extends Controller {
      */
     public function __construct()
     {
-
+        $this->middleware('admin', ['only' => ['getCreate', 'postCreate']]);
     }
 
     public function getIndex()
@@ -23,12 +27,20 @@ class SchoolController extends Controller {
 
     public function getCreate()
     {
-
+        return view('create_school');
     }
 
-    public function postCreate()
+    public function postCreate(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required|unique:schools|max:255'
+        ]);
 
+        $school = new School;
+        $school->name = $request->input('name');
+        $school->save();
+
+        return Redirect::back()->with('success', 'School was succesfully created!');
     }
 
     public function getUpdate($id)
---
+++ resources/views/create_school.blade.php
@@ -0,0 +1,50 @@
+@extends('app')
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">Create School</div>
+				<div class="panel-body">
+					@if (count($errors) > 0)
+						<div class="alert alert-danger">
+							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+
+					@if (Session::has('success'))
+						<div class="alert alert-success">
+							{{ Session::get('success') }}
+						</div>
+					@endif
+
+					<form class="form-horizontal" role="form" method="POST" action="{{ url('/schools/create') }}">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+						<div class="form-group">
+							<label class="col-md-4 control-label">Name</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-6 col-md-offset-4">
+								<button type="submit" class="btn btn-primary">
+									Create School
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection

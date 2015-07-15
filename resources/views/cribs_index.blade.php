04629024d895a5ffbfc3e96344ea0a356c0853e0
Carson Killbreath <kill4992@kettering.edu>
2015-07-14 20:07:29 -0400

    Added upload button to Cribs page.

--- resources/views/cribs_index.blade.php
+++ resources/views/cribs_index.blade.php
@@ -4,23 +4,26 @@
 <div class="container">
 	<div class="row">
         <div class="col-md-4">
+
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <div class="form-group">
                         <input type="text" class="form-control" name="" placeholder="Search courses..." />
-                    </div>
                 </div>
             </div>
+
             <ul class="list-group">
 
             </ul>
+
         </div>
         <div class="col-md-8">
+
     		<div class="panel panel-default">
                 <div class="panel-heading">
                     Cribs
                 </div>
                 <div class="panel-body">
+
                     @forelse($cribs as $key => $crib)
                         <h4>
                             {{ $crib->name }}
@@ -28,12 +31,14 @@
                         </h4>
                         <p>{{ $crib->description }}</p>
 
-                        @if(($key + 1) < count($cribs))
-                         <hr />
-                        @endif
+                        <hr />
                     @empty
                         <div class="alert alert-info" role="alert">No results found!</div>
                     @endforelse
+
+                    <div class="clearfix">
+                        <a href="{{ url('cribs/create') }}" class="btn btn-primary pull-right">Upload Crib</a>
+                    </div>
                 </div>
             </div>
         </div>
